6e9e3e7f5940404cc09437456fc7dc30d97c0da0
Mykhailo Pastukh <Mykhailo.Pastukh@udelphi.com>
2019-05-08 17:31:28 +0300

    "#2 add node"

---
+++ DataStructure/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Node
+    {
+        private Node _next;
+        private Node _previous;
+        private int _item;
+        public int GetItem()
+        {
+            return _item;
+        }
+        public void SetItem(int item)
+        {
+            _item = item;
+        }
+        public Node GetNext()
+        {
+            return _next;
+        }
+        public void SetNext(Node next)
+        {
+            _next = next;
+        }
+        public Node GetPrevious()
+        {
+            return _previous;
+        }
+        public void SetPrevious(Node previous)
+        {
+            _previous = previous;
+        }
+    }
+}
