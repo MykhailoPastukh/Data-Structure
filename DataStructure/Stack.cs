38ec0f3676e132f2931a8c5606a399036ca083fd
Mykhailo Pastukh <Mykhailo.Pastukh@udelphi.com>
2019-05-08 17:51:12 +0300

    "#2 Implement Stack"

---
+++ DataStructure/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Stack : IStack
+    {
+        private Node _first;
+        private int _size;
+
+        public void Add(Node node)
+        {
+            if (_first == null)
+            {
+                _first = node;
+            }
+            else
+            {
+                node.SetNext(_first);
+                _first.SetPrevious(node);
+                _first = node;
+            }
+            _size++;
+        }
+
+        public Node Get()
+        {
+            Node result = _first;
+            _first = _first.GetNext();
+            _size--;
+            result.SetNext(null);
+            return result;
+        }
+
+        public void Clear()
+        {
+            _first = null;
+            _size = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            if (_first == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+    }
+}
