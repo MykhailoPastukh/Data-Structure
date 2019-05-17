942ad7df3d34b223a38ab80380b14ca2cc5f6ea0
Mykhailo Pastukh <Mykhailo.Pastukh@udelphi.com>
2019-05-17 18:26:55 +0300

    "#15 Add LinkedList-based RingBuffer and tests"

---
+++ DataStructure.Test/RingBufferArrayTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Xunit;
+
+namespace DataStructure.Tests
+{
+    public class RingBufferArrayTests
+    {
+        [Fact]
+        public void IsEmpty_EmptyRingBufferShouldReturnTrue()
+        {
+            // Arrange
+            RingBufferArray sut = new RingBufferArray(5);
+            bool expected = true;
+
+            // Act
+            sut.Add(42);
+            sut.Get();
+            bool actual = sut.IsEmpty();
+
+            // Assert
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Clear_ClearRingBufferWithSomeNumbersShouldWork()
+        {
+            // Arrange
+            RingBufferArray sut = new RingBufferArray(10);
+            int expected = 0;
+
+            // Act
+            sut.Add(int.MaxValue);
+            sut.Add(int.MinValue);
+            sut.Add(42);
+            sut.Clear();
+            int actual = sut.Size();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Add_RingBuferShouldOverwriteWhenItFull()
+        {
+            // Arrange
+            RingBufferArray sut = new RingBufferArray(7);
+            int expected = 1;
+
+            // Act
+            for (int i = 0; i < 8; i++)
+            {
+                sut.Add(i);
+            }
+            int actual = sut.Get();
+
+            // Assert
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Get_GetFromEmptyRingBufferShouldNotWork()
+        {
+            // Arrange
+            RingBufferArray sut = new RingBufferArray(10);
+
+            // Act
+            sut.Add(42);
+            sut.Get();
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(()=>sut.Get());
+        }
+
+        [Fact]
+        public void Get_GetAllNumbersFromRingBufferShouldWork()
+        {
+            // Arrange
+            RingBufferArray sut = new RingBufferArray(10);
+            int expected = 0;
+
+            // Act
+            for (int i = 0; i < 20; i++)
+            {
+                sut.Add(i);
+            }
+            for (int i = 0; i <= 10; i++)
+            {
+                sut.Get();
+            }
+            int actual = sut.Size();
+
+            // Assert
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void RingBufferArray_CreateRingBufferWithNegativeSizeShouldNotWork()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => new RingBufferArray(-1));
+        }
+    }
+}
--- DataStructure.Test/RingBufferTests.cs
+++ DataStructure.Test/RingBufferTests.cs
@@ -1,20 +1,56 @@
 ﻿using System;
 using Xunit;
 
-namespace DataStructure.Tests
+namespace DataStructure.Test
 {
     public class RingBufferTests
     {
         [Fact]
-        public void IsEmpty_EmptyRingBufferShouldReturnTrue()
+        public void Add_AddSomeNumbersToRingBufferShouldWork()
         {
             // Arrange
-            RingBufferArray sut = new RingBufferArray(5);
+            RingBuffer sut = new RingBuffer(3);
+            int expected = 3;
+
+            // Act
+            sut.Add(11);
+            sut.Add(12);
+            sut.Add(13);
+            int actual = sut.Size();
+
+            // Assert
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Add_AddNumbersToFullRingBufferShouldNotWork()
+        {
+            // Arrange
+            RingBuffer sut = new RingBuffer(5);
+
+            // Act
+            for (int i = 0; i < 5; i++)
+            {
+                sut.Add(i);
+            }
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(()=>sut.Add(6));
+        }
+
+        [Fact]
+        public void Clear_ClearRingBufferShouldWork()
+        {
+            // Arrange
+            RingBuffer sut = new RingBuffer(100);
             bool expected = true;
 
             // Act
-            sut.Add(42);
-            sut.Get();
+            for (int i = 0; i < 100; i++)
+            {
+                sut.Add(i);
+            }
+            sut.Clear();
             bool actual = sut.IsEmpty();
 
             // Assert
@@ -22,86 +58,65 @@ namespace DataStructure.Tests
         }
 
         [Fact]
-        public void Clear_ClearRingBufferWithSomeNumbersShouldWork()
+        public void Get_GetNumbersFromEmptyRingBufferShouldNotWork()
         {
             // Arrange
-            RingBufferArray sut = new RingBufferArray(10);
-            int expected = 0;
+            RingBuffer sut = new RingBuffer(5);
 
             // Act
-            sut.Add(int.MaxValue);
-            sut.Add(int.MinValue);
-            sut.Add(42);
-            sut.Clear();
-            int actual = sut.Size();
 
+            // Assert
+            Assert.Throws<NullReferenceException>(() => sut.Get());
+        }
+
+        [Fact]
+        public void Get_AddAndGetSomeNumbersShouldWork()
+        {
+            // Arrange
+            RingBuffer sut = new RingBuffer(5);
+            int expected = 42;
+
+            // Act
+            sut.Add(12);
+            sut.Add(15);
+            sut.Add(42);
+            sut.Get();
+            sut.Add(48);
+            sut.Get();
+            int actual = sut.Get();
             // Assert
             Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void Add_RingBuferShouldOverwriteWhenItFull()
+        public void RingBuffer_CreateRingBufferWithNegativeSizeShouldNotWork()
         {
             // Arrange
-            RingBufferArray sut = new RingBufferArray(7);
-            int expected = 1;
 
             // Act
-            for (int i = 0; i < 8; i++)
-            {
-                sut.Add(i);
-            }
-            int actual = sut.Get();
 
             // Assert
-            Assert.Equal(expected,actual);
+            Assert.Throws<ArgumentException>(()=>new RingBuffer(-2));
         }
 
         [Fact]
-        public void Get_GetFromEmptyRingBufferShouldNotWork()
+        public void Expand_ExpandRingBufferShouldWork()
         {
             // Arrange
-            RingBufferArray sut = new RingBufferArray(10);
+            RingBuffer sut = new RingBuffer(3);
+            int expected = 5;
 
             // Act
-            sut.Add(42);
-            sut.Get();
-
-            // Assert
-            Assert.Throws<InvalidOperationException>(()=>sut.Get());
-        }
-
-        [Fact]
-        public void Get_GetAllNumbersFromRingBufferShouldWork()
-        {
-            // Arrange
-            RingBufferArray sut = new RingBufferArray(10);
-            int expected = 0;
-
-            // Act
-            for (int i = 0; i < 20; i++)
-            {
-                sut.Add(i);
-            }
-            for (int i = 0; i <= 10; i++)
-            {
-                sut.Get();
-            }
+            sut.Add(1);
+            sut.Add(2);
+            sut.Add(3);
+            sut.Expand(2);
+            sut.Add(4);
+            sut.Add(5);
             int actual = sut.Size();
 
             // Assert
-            Assert.Equal(expected,actual);
-        }
-
-        [Fact]
-        public void RingBufferArray_CreateRingBufferWithNegativeSizeShouldNotWork()
-        {
-            // Arrange
-
-            // Act
-
-            // Assert
-            Assert.Throws<ArgumentException>(() => new RingBufferArray(-1));
+            Assert.Equal(expected, actual);
         }
     }
 }
