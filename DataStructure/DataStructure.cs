ba62479b2a165a8c8fa87c395587ee31745ac0ce
Mykhailo Pastukh <Mykhailo.Pastukh@udelphi.com>
2019-05-28 17:40:42 +0300

    "#1 fix dataStructure class"

--- DataStructure/DataStructure.cs
+++ DataStructure/DataStructure.cs
@@ -3,7 +3,7 @@
 namespace DataStructure
 {
     public delegate void DataStructureEventHandler<T>(IDataStructure<T> sender, EventArgs e);
-    public class DataStructure<T> : IDataStructure<T>
+    public abstract class DataStructure<T> : IDataStructure<T>
     {
         public event DataStructureEventHandler<T> AddElement;
         public event DataStructureEventHandler<T> RemoveElement;
@@ -27,25 +27,10 @@ namespace DataStructure
             ContainerFull?.Invoke(this, e);
         }
 
-        public virtual void Add(T item)
-        {
-            throw new NotImplementedException();
-        }
-        public virtual T Get()
-        {
-            throw new NotImplementedException();
-        }
-        public virtual void Clear()
-        {
-            throw new NotImplementedException();
-        }
-        public virtual bool IsEmpty()
-        {
-            throw new NotImplementedException();
-        }
-        public virtual int Size()
-        {
-            throw new NotImplementedException();
-        }
+        public abstract void Add(T item);
+        public abstract T Get();
+        public abstract void Clear();
+        public abstract bool IsEmpty();
+        public abstract int Size();
     }
 }
